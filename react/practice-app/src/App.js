b53965544fd5eca7e0fa5c741c1bb1c627a134c1
Summer Brown <summer-brown@comcast.net>
2019-11-06 19:48:34 -0600

    practice

---
+++ react/practice-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './Timer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer maxNum={7} />
+    </div>
+  );
+}
+
+export default App;
